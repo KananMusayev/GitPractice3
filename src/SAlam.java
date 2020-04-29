a9a86093412fcc17d61ef6fcf5da49e2c666eda2
Kanan <64110986+KananMusayev@users.noreply.github.com>
2020-04-29 06:17:56 -0400

    Update SAlam.java

--- src/SAlam.java
+++ src/SAlam.java
@@ -1,7 +1,9 @@
 public class SAlam {
     public static void main(String[] args) {
         System.out.println("Dunya senin dunya menim dunya heshkimin");
-        System.out.println("bele ozun necesen atam?")
+        System.out.println("bele ozun necesen atam?");
+        System.out.println("Ben bole dersin ta ana avradini :D");
+
         
     }
 }
