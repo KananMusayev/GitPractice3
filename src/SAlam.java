7ac198d90aceefe77bd5336045a949d9947b6d2e
Kanan Musayev <kanan.musayev@lesroches.ch>
2020-04-29 06:20:24 -0400

    Merge branch 'master' of /Users/kenanmusayev/IdeaProjects/GitPractice3 with conflicts.

--- src/SAlam.java
+++ src/SAlam.java
@@ -1,9 +1,7 @@
 public class SAlam {
     public static void main(String[] args) {
         System.out.println("Dunya senin dunya menim dunya heshkimin");
-        System.out.println("bele ozun necesen atam?");
-        System.out.println("Ben bole dersin ta ana avradini :D");
-
+        System.out.println("bele ozun necesen atam?")
         
     }
 }
